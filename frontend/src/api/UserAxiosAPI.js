877b0cd05554998d3e5e8569b065d213f91fd21f
lamsasa <dh85311@gmail.com>
2023-07-13 01:51:39 +0900

    ✨이메일 전송 api 연결

--- frontend/src/api/UserAxiosAPI.js
+++ frontend/src/api/UserAxiosAPI.js
@@ -2,22 +2,30 @@ import axios from "axios";
 const DOMAIN = "https://localhost:8888";
 
 const UserAxiosAPI = {
-    getUserInfo : async() => {
-        try {
-            return await axios.get(DOMAIN + "/user/me", {withCredentials: true});
-        } catch (e) {
-            console.log("getUserInfo : " + e)
-        }
-    },
-
-    postEmailSend : async(inputEmail) => {
-        try {
-            const response = await axios.post(DOMAIN + "/mail/sendmail", inputEmail, {withCredentials: true});
-            return response.data;
-        } catch (e) {
-            console.log("postEmailSend : " + e)
-        }
+  getUserInfo: async () => {
+    try {
+      return await axios.get(DOMAIN + "/user/me", { withCredentials: true });
+    } catch (e) {
+      console.log("getUserInfo : " + e);
     }
-}
+  },
+
+  postEmailSend: async (inputEmail) => {
+    try {
+        const response = await axios.post(
+            DOMAIN + "/mail/sendmail",
+            {
+                type: "findPw",
+                email: inputEmail,
+            },
+            { withCredentials: true }
+        );
+        return response.data;
+    } catch (error) {
+        console.log("postEmailSend: " + error);
+        throw error;
+    }
+},
+};
 
 export default UserAxiosAPI;
