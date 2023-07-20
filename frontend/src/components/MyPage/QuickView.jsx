4225defbec7088a80bd6e93c14bde26fcef80369
Soomin <o_v_zzza@naver.com>
2023-07-21 00:00:46 +0900

    🎨 간편등록 스타일 수정

--- frontend/src/components/MyPage/QuickView.jsx
+++ frontend/src/components/MyPage/QuickView.jsx
@@ -1,81 +1,78 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import styled from "styled-components";
 import BlockLine from "../Common/BlockLine";
 import Box from "../Common/Box";
 import Tag from "./Tag";
-import ClickButton from "../Common/ClickButton";
+// import ClickButton from "../Common/ClickButton";
 
 import QuickAxiosApi from "../../api/QuickAddAxiosAPI";
 
-const QuickView = ({isBasic, data, setData, close} ) => {
+const QuickView = ({ isBasic, data, setData, close }) => {
+  const [myPageList, setMyPageList] = useState([]);
 
-    const [myPageList, setMyPageList] = useState([]);
+  // mypage의 data를
+  const onClickQuickMenu = (data) => {
+    console.log(data);
+    setData(data);
+    close();
+  };
 
-    // mypage의 data를
-    const onClickQuickMenu = (data) => {
-        console.log(data);
-        setData(data);
-        close();
-    }
+  useEffect(() => {
+    const getMyPageList = async () => {
+      try {
+        const rsp = await QuickAxiosApi.getMyPageList();
+        if (rsp.status === 200) setMyPageList(rsp.data);
+        setMyPageList(rsp.data);
+        console.log("마이페이지 list 조회");
+      } catch (error) {
+        console.error("Request Error:", error);
+      }
+    };
+    getMyPageList();
+  }, []);
 
-    useEffect(() => {
-        const getMyPageList = async () => {
-            try {
-                const rsp = await QuickAxiosApi.getMyPageList();
-                if (rsp.status === 200) setMyPageList(rsp.data);
-                setMyPageList(rsp.data);
-                console.log("마이페이지 list 조회");
-            } catch (error) {
-                console.error("Request Error:", error);
-            }
-        };
-        getMyPageList();
-    }, []);
+  return (
+    <>
+      <Title>간편 등록</Title>
+      <BlockLine />
+      <QuickViewContainer>
+        {isBasic ? (
+          <>
+            {myPageList.myScheduleDtoList &&
+              myPageList.myScheduleDtoList.map((data1) => (
+                <div onClick={() => onClickQuickMenu(data1)} key={data1.id}>
+                  <Tag
+                    width={"20%"}
+                    color={data1.colorId}
+                    detail={data1.scName}
+                  />
+                </div>
+              ))}
+          </>
+        ) : (
+          <>
+            {myPageList.myWorkDtoList &&
+              myPageList.myWorkDtoList.map((data2) => (
+                <div onClick={() => onClickQuickMenu(data2)} key={data2.id}>
+                  <Tag
+                    width={"20%"}
+                    color={data2.colorId}
+                    detail={data2.workName}
+                  />
+                </div>
+              ))}
+          </>
+        )}
+      </QuickViewContainer>
 
-
-
-    return (
-        <>
-            <Title>간편 등록</Title>
-            <BlockLine/>
-            <Box height={"60%"}>
-                {isBasic ? (
-                    <>
-                        {myPageList.myScheduleDtoList &&
-                            myPageList.myScheduleDtoList.map((data1) => (
-                                <div onClick={() => onClickQuickMenu(data1)} key={data1.id}>
-                                    <Tag
-                                        width={"20%"}
-                                        color={data1.colorId}
-                                        detail={data1.scName}
-                                    />
-                                </div>
-                            ))}
-                    </>
-                ) : (
-                    <>
-                        {myPageList.myWorkDtoList &&
-                            myPageList.myWorkDtoList.map((data2) => (
-                                <div onClick={() => onClickQuickMenu(data2)} key={data2.id}>
-                                    <Tag
-                                        width={"20%"}
-                                        color={data2.colorId}
-                                        detail={data2.workName}
-                                    />
-                                </div>
-                            ))}
-                    </>
-                )}
-            </Box>
-
-            {/*<ButtonContainer>*/}
-            {/*    <ClickButton onClick={onChangeValue} width={"100px"} height={"35px"}>*/}
-            {/*        선택하기*/}
-            {/*    </ClickButton>*/}
-            {/*</ButtonContainer>*/}
-        </>
-    );
+      {/*<ButtonContainer>*/}
+      {/*    <ClickButton onClick={onChangeValue} width={"100px"} height={"35px"}>*/}
+      {/*        선택하기*/}
+      {/*    </ClickButton>*/}
+      {/*</ButtonContainer>*/}
+    </>
+  );
 };
 
 export default QuickView;
@@ -87,9 +84,8 @@ const Title = styled.div`
   margin: 20px 10px;
 `;
 
-const ButtonContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 20px;
+const QuickViewContainer = styled.div`
+  width: "90%";
+  margin: 25px auto;
+  margin-bottom: 20px;
 `;
